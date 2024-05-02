9f44220ce2a3652faefd9abd3091778e90207ec7
Arcedo <arcedo.marc@gmail.com>
2024-05-02 10:49:58 +0200

    [ADD] all fetches in one file

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import GridProjectSection from "../components/GridProjectSection";
 import Header from "../components/Header";
@@ -6,36 +6,27 @@ import Slider from "../components/Slider";
 import Footer from "../components/Footer";
 import CategorySlider from "../components/CategorySlider";
 import RandomSection from "../components/RandomSection";
+import { getLatestsProjects, getRandomProjects } from "../api/apiCalls";
 
-const server = import.meta.env.VITE_API_URL;
-
-async function getLatestsProjects(skip, limit) {
-    return await (await fetch(`${server}projects?startIndex=${skip}&limit=${limit}`, {
-        method: 'GET',
-        mode: 'cors'
-    })).json();
-}
-
-// async function getProjectsByCategory(idCategory, skip, limit) {
-//     return await (await fetch(`${server}projects/byCategory/${idCategory}?startIndex=${skip}&limit=${limit}`, {
-//         method: 'GET',
-//         mode: 'cors'
-//     })).json();
-// }
-
-async function getRandomProjects(skip, limit) {
-    return await (await fetch(`${server}projects/random?startIndex=${skip}&limit=${limit}`, {
-        method: 'GET',
-        headers: new Headers({ 'Content-type': 'application/json' }),
-        mode: 'cors'
-    })).json();
-}
-
-const latestsProjects = await getLatestsProjects(0, 4);
-const byInterestProjects = await getRandomProjects(0, 4);
-const popularProjects = await getRandomProjects(0, 4);
-console.log(byInterestProjects);
 function Home() {
+    const [latestsProjects, setLatestsProjects] = useState([]);
+    const [byInterestProjects, setByInterestProjects] = useState([]);
+    const [popularProjects, setPopularProjects] = useState([]);
+
+    useEffect(() => {
+        const fetchProjects = async () => {
+            const latestProjectsData = await getLatestsProjects(0, 4);
+            const byInterestProjectsData = await getRandomProjects(0, 4);
+            const popularProjectsData = await getRandomProjects(0, 4);
+
+            setLatestsProjects(latestProjectsData);
+            setByInterestProjects(byInterestProjectsData);
+            setPopularProjects(popularProjectsData);
+        };
+
+        fetchProjects();
+    }, []);
+
     return (
         <div className=" w-full bg-white min-h-screen overflow-hidden h-fit flex flex-col gap-16">
             <Header />
