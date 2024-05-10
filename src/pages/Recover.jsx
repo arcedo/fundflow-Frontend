5522bcf64137f56bbb1a30ee33be6b5d970027be
Arcedo <arcedo.marc@gmail.com>
2024-05-10 13:14:41 +0200

    [ADD] recover

--- src/pages/Recover.jsx
+++ src/pages/Recover.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 import UserMain from '../components/UserMain';
 
-function Recover(){
+function Recover() {
     return (
         <UserMain>
             <form className="flex flex-col w-64 gap-4" action="">
@@ -12,9 +12,9 @@ function Recover(){
                 </div>
                 <div className='w-full'>
                     <label htmlFor="email" className='text-black font-semibold tracking-tighter opacity-70'>Email</label>
-                    <input id='email' className="p-2 bg-white rounded-lg font-montserrat border border-gray-500 border-opacity-30 w-full text-black outline-none focus:border-opacity-80 transition-all duration-200" type="text"/>
+                    <input id='email' className="p-2 bg-white rounded-lg font-montserrat border border-gray-500 border-opacity-30 w-full text-black outline-none focus:border-opacity-80 transition-all duration-200" type="text" />
                 </div>
-                <button className="p-2 mt-2 mb-4 bg-secondary bg-opacity-70 rounded-lg text-white font-bold" type="submit">Send email</button>              
+                <button className="p-2 mt-2 mb-4 bg-secondary bg-opacity-70 rounded-lg text-white font-bold" type="submit">Send email</button>
             </form>
             <Link to={"/login"} className='text-primary font-normal tracking-tight w-full transition-all duration-200 hover:text-purple-500'>Go back to login</Link>
         </UserMain>
