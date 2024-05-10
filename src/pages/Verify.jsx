fa342505e2b65f427048b218f32501a24ab77ea1
enricarmengol <enarga@jviladoms.cat>
2024-05-10 09:45:14 +0200

    [FIX] redirect logic for already verified users

--- src/pages/Verify.jsx
+++ src/pages/Verify.jsx
@@ -7,15 +7,21 @@ import emailVerified from '../assets/icons/emailVerified.svg';
 function Verify() {
     let navigate = useNavigate();
     const { code } = useParams();
+    let redirect = true;
     const [verification, setVerification] = useState(null);
     useEffect(() => {
+        const userData = JSON.parse(localStorage.getItem('userData'));
+        if (userData && userData.verifiedEmail) {
+            navigate('/');
+        }
         const verify = async () => {
             const response = await endVerificationEmail(code);
             setVerification(response);
-            if (response && response.message) {
+            if (response && response.message && redirect) {
                 if (localStorage.getItem('userData')) {
+                    // TODO: Verify if the user logged is the same as the one that is being verified
                     const currentUser = JSON.parse(localStorage.getItem('userData'));
-                    currentUser.verified = true;
+                    currentUser.verifiedEmail = true;
                     localStorage.setItem('userData', JSON.stringify(currentUser));
                 }
                 setTimeout(() => {
@@ -24,10 +30,10 @@ function Verify() {
             }
         }
         verify();
-    }, []);
+    }, [navigate, code]); 
     return (
         <UserMain>
-            {verification.code === 200 ? (
+            {verification ? (verification.code === 200 ? (
                 <div className='text-black flex flex-col justify-center items-center gap-2'>
                     <h2 className='text-xl font-bold'>Email verified!</h2>
                     <img src={emailVerified} alt='Email verified' />
@@ -38,7 +44,11 @@ function Verify() {
                     <p className='opacity-70'>Seems something failed. Want to try again?</p>
                     <button className='p-3 bg-gradient-to-r from-primary to-secondary border-none bg-opacity-50 rounded-lg text-white font-bold'>Resend email</button>
                 </div>
-            )}
+            )): 
+            <div className='text-black flex flex-col justify-center items-center gap-2'>
+            <h2 className='text-xl font-bold'>Verifying your account...</h2>
+        </div>
+        }
         </UserMain>
     );
 }
