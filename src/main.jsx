6c537dcee703d293abc6567584b4c179abb02c0c
Enric Armengol <enarga@jviladoms.cat>
2024-05-24 18:57:36 +0200

    [FIX] google client id update

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import App from './App.jsx'
-import { render } from 'react-dom';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <GoogleOAuthProvider clientId="881197720936-bdil759nqiocbne2b6sd3i6k4ujicss1.apps.googleusercontent.com">
+  <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
     <App />
   </GoogleOAuthProvider>
 )
